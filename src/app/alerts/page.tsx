63e3ca08cddd6e6cd0671e6854c7008065068da9
asiripr <asiripramodaya2000@gmail.com>
2025-02-08 19:46:08 +0530

    alerts + solutions

--- src/app/alerts/page.tsx
+++ src/app/alerts/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Footer from "../components/Footer";
+import { useRouter } from 'next/navigation';
 
 const About = () => {
+    const router = useRouter();
     return (
         <>
             <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center px-4">
@@ -10,13 +13,15 @@ const About = () => {
                 </h2>
 
                 {/* Image */}
-                <div
+                <img
+                    src="/images/tea-leave.png"
+                    alt="tea_leave"
                     className="w-80 md:w-96 h-48 md:h-56 bg-cover bg-center rounded-lg shadow-md mb-6"
-                    style={{ backgroundImage: "url(/image.png)" }}
-                ></div>
+                />
 
                 {/* Button */}
                 <button
+                    onClick={() => router.push("/solutions")}
                     className="bg-green-700 hover:bg-green-800 text-white text-lg font-medium px-6 py-3 rounded-full transition-all duration-300 shadow-md"
                 >
                     Get The Solutions
